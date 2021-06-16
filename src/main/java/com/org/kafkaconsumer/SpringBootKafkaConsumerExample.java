25381a61f8ccc352f7bff738b7ff98c04bee1b64
Rajat Gundi <glrajat@gmail.com>
2021-06-16 11:22:37 +0530

    Added: kafka-consumer and its config

---
+++ src/main/java/com/org/kafkaconsumer/SpringBootKafkaConsumerExample.java
@@ -0,0 +1,12 @@
+package com.org.kafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaConsumerExample {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootKafkaConsumerExample.class);
+    }
+}
